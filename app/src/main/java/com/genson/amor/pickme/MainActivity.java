dcdc72ed29f3de63a88ba4a82f6007aec9415210
amor genson <maleahamorgenson@gmail.com>
2017-08-26 09:11:12 +0800

    Final Commit

--- app/src/main/java/com/genson/amor/pickme/MainActivity.java
+++ app/src/main/java/com/genson/amor/pickme/MainActivity.java
@@ -1,47 +1,70 @@
 package com.genson.amor.pickme;
 
-import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
-import java.io.FileNotFoundException;
-
-import static com.genson.amor.pickme.R.drawable.camera;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final int PICK_PHOTO_FOR_AVATAR = camera ;
+    private static int PICK_PHOTO_REQUEST = 1;
+
+    ImageView viewedImg;
+    TextView pickText;
+    Button shareBtn;
+    Uri uri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViews();
     }
-    public void pickImage() {
-        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-        intent.setType("image/*");
-        startActivityForResult(intent, PICK_PHOTO_FOR_AVATAR);
+
+    public void findViews() {
+
+        viewedImg = (ImageView) findViews(R.id.viewed_photo);
+        pickText = (TextView) findViewById(R.id.camera_text);
+        shareBtn = (Button) findViewById(R.id.share_button);
+    }
+
+    public void pickPhoto(View v) {
+        Intent gallery = new Intent(Intent.ACTION_PICK);
+        gallery.setType("image/*");
+        startActivityForResult(gallery, PICK_PHOTO_REQUEST);
+    }
+
+    public void sharePhoto(View v) {
+        Intent share = new Intent(Intent.ACTION_SEND);
+        share.setType("image/*");
+        share.setData(uri);
+        share.putExtra(Intent.EXTRA_STREAM, uri);
+        startActivity(Intent.createChooser(share, getString(R.string.share_text_)));
     }
 
     @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == PICK_PHOTO_FOR_AVATAR && resultCode == Activity.RESULT_OK) {
-            if (data == null) {
-                //Display an error
-                return;
-            }
-            Context context = null;
+
+        if (requestCode == PICK_PHOTO_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            uri = data.getData();
             try {
-                assert false;
-                context.getContentResolver().openInputStream(data.getData());
-            } catch (FileNotFoundException e) {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+                viewedImg.setImageBitmap(bitmap);
+                viewedImg.setVisibility(View.VISIBLE);
+                shareBtn.setVisibility(View.VISIBLE);
+                pickText.setVisibility(View.INVISIBLE);
+            } catch (IOException e) {
                 e.printStackTrace();
             }
-            //Now you can do whatever you want with your inpustream, save it as file, upload to a server, decode a bitmap...
         }
     }
-
 }
